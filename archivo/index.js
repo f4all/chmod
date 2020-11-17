afbf03ffc2a2278d954098d08eefc5da36e19e68
f4all <54265436+f4all@users.noreply.github.com>
2020-11-17 18:47:41 -0300

    Update index.js

--- archivo/index.js
+++ archivo/index.js
@@ -1,39 +1,40 @@
-'use strict';
-
 /**
  * gh-index
  * An directory index for gh-pages.
  */
 
-window.addEventListener('DOMContentLoaded', function () {
-  var wrapper = document.getElementById('gh-index');
+window.addEventListener('DOMContentLoaded', () => {
+  const wrapper = document.getElementById('gh-index')
 
-  function insertStylesheet(url) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = url;
-    document.head.appendChild(link);
+  function insertStylesheet (url) {
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    link.href = url
+    document.head.appendChild(link)
   }
 
-  function insertStylesheetAsync(url) {
-    var raf = function () {
-      return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-      };
-    }();
+  function insertStylesheetAsync (url) {
+    const raf = (function () {
+      return window.requestAnimationFrame ||
+             window.webkitRequestAnimationFrame ||
+             window.mozRequestAnimationFrame ||
+             function (callback) {
+               window.setTimeout(callback, 1000 / 60)
+             }
+    })()
     raf(function () {
-      insertStylesheet(url);
-    });
+      insertStylesheet(url)
+    })
   }
 
-  function insertScript(url) {
-    var script = document.createElement('script');
-    script.src = url;
-    document.head.appendChild(script);
+  function insertScript (url) {
+    const script = document.createElement('script')
+    script.src = url
+    document.head.appendChild(script)
   }
 
-  var index = {
+  const index = {
 
     // RegExp for files to exclude
     excludes: new RegExp(wrapper.getAttribute('excludes')),
@@ -41,65 +42,66 @@ window.addEventListener('DOMContentLoaded', function () {
     /**
      * Init gh-index
      */
-    init: function init() {
-      window.addEventListener('hashchange', index.hashRoute);
+    init: function () {
+      window.addEventListener('hashchange', index.hashRoute)
 
-      insertStylesheet('index.css');
+      insertStylesheet('http://amio.github.io/gh-index/index.css')
       // insertStylesheet('index.css')
-      insertStylesheetAsync('octicons.css');
-      window.fetch || insertScript('https://cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js');
+      insertStylesheetAsync('https://octicons.github.com/components/octicons/octicons/octicons.css')
+      window.fetch || insertScript('https://cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js')
 
-      index.loadTrees();
+      index.loadTrees()
     },
 
-    getRepoInfo: function getRepoInfo() {
-      var config = wrapper.getAttribute('repo') || wrapper.getAttribute('data-repo');
-      if (!config) return null;
+    getRepoInfo: function () {
+      const config = wrapper.getAttribute('repo') || wrapper.getAttribute('data-repo')
+      if (!config) return null
 
-      var repoInfo = config.split('/');
+      const repoInfo = config.split('/')
       return {
         owner: repoInfo[0],
         name: repoInfo[1],
         branch: 'gh-pages'
-      };
+      }
     },
 
-    loadTrees: function loadTrees() {
-      var repo = index.getRepoInfo();
-      if (!repo) return window.alert('Repo config missing!');
+    loadTrees: function () {
+      const repo = index.getRepoInfo()
+      if (!repo) return window.alert('Repo config missing!')
 
-      var uri = 'https://api.github.com/repos/' + repo.owner + '/' + repo.name + ('/git/trees/' + repo.branch + '?recursive=1');
-      window.fetch(uri, { cache: 'force-cache' }).then(function (resp) {
-        return resp.json();
-      }).then(function (result) {
-        index.refresh(result.tree);
-      });
+      const uri = `https://api.github.com/repos/${repo.owner}/${repo.name}` +
+        `/git/trees/${repo.branch}?recursive=1`
+      window.fetch(uri, {cache: 'force-cache'})
+        .then(resp => resp.json())
+        .then(result => {
+          index.refresh(result.tree)
+        })
     },
 
     /**
      * Everything after data loaded.
      * @param resp
      */
-    refresh: function refresh(treeData) {
+    refresh: function (treeData) {
       // build tree
-      index.tree = index.genTree(treeData);
+      index.tree = index.genTree(treeData)
 
       // build html
-      index.hashRoute();
+      index.hashRoute()
     },
 
     /**
      * Route task depends on current hash
      */
-    hashRoute: function hashRoute() {
-      var path = window.location.hash.substr(1).split('/');
-      var sub = index.tree;
+    hashRoute: function () {
+      var path = window.location.hash.substr(1).split('/')
+      var sub = index.tree
 
       while (sub && path.length && path[0]) {
-        sub = sub[path.shift()];
+        sub = sub[path.shift()]
       }
 
-      index.updateIndexies(sub);
+      index.updateIndexies(sub)
     },
 
     /**
@@ -107,15 +109,15 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param {Array} treeArr
      * @returns {Object}
      */
-    genTree: function genTree(treeArr) {
-      var root = {};
+    genTree: function (treeArr) {
+      var root = {}
       for (var i = treeArr.length; i--;) {
         if (!this.excludes.test(treeArr[i].path)) {
-          this.addItem(root, treeArr[i]);
+          this.addItem(root, treeArr[i])
         }
       }
 
-      return root;
+      return root
     },
 
     /**
@@ -124,59 +126,64 @@ window.addEventListener('DOMContentLoaded', function () {
      * @param item
      * @returns {*}
      */
-    addItem: function addItem(tree, item) {
-      var parent = tree;
+    addItem: function (tree, item) {
+      var parent = tree
       item.path.replace(/[^/]+/g, function (seg, idx) {
-        parent[seg] || (parent[seg] = {});
-        parent = parent[seg];
-        return idx;
-      });
+        parent[seg] || (parent[seg] = {})
+        parent = parent[seg]
+        return idx
+      })
 
-      parent['/NODE/'] = item;
+      parent['/NODE/'] = item
 
-      return item;
+      return item
     },
 
     /**
      * Update list base on tree
      * @param tree
      */
-    updateIndexies: function updateIndexies(tree) {
+    updateIndexies: function (tree) {
       // generate header html
-      var path = window.location.hash.replace('#', '');
-      var header = '<div class="header"><span>•_•</span></div>';
-      var parentLink = '#' + path.replace(/[^/]+\/$/, '');
+      var path = window.location.hash.replace('#', '')
+      var header = '<div class="header"><span>•_•</span></div>'
+      var parentLink = '#' + path.replace(/[^/]+\/$/, '')
       if (path) {
-        header = '<div class="header"><a class="uplink" href="' + parentLink + '">' + (path + '<span>←_←</span></a></div>');
+        header = `<div class="header"><a class="uplink" href="${parentLink}">` +
+          `${path}<span>←_←</span></a></div>`
       }
 
       // generate list html
-      var repo = index.getRepoInfo();
-      var home = 'http://' + repo.owner + '.github.io/' + repo.name + '/';
-      var items = Object.keys(tree).map(function (key) {
-        if (key === '/NODE/') return '';
+      const repo = index.getRepoInfo()
+      const home = 'http://' + repo.owner + '.github.io/' + repo.name + '/'
+      const items = Object.keys(tree).map(key => {
+        if (key === '/NODE/') return ''
 
-        var node = tree[key]['/NODE/'];
-        var str = '';
+        let node = tree[key]['/NODE/']
+        let str = ''
         switch (node.type) {
           case 'blob':
-            str = '<li class="blob"><a href="' + (home + node.path) + '">' + ('<span class="octicon octicon-file-text"></span> ' + node.path + '</a></li>');
-            break;
+            str = `<li class="blob"><a href="${home + node.path}">` +
+              `<span class="octicon octicon-file-text"></span> ${node.path}</a></li>`
+            break
           case 'tree':
-            str = '<li class="tree"><a href="#' + node.path + '/">' + ('<span class="octicon octicon-file-directory"></span> ' + node.path + '/</a></li>');
-            break;
+            str = `<li class="tree"><a href="#${node.path}/">` +
+              `<span class="octicon octicon-file-directory"></span> ${node.path}/</a></li>`
+            break
         }
-        return str;
-      }).join('');
-      var list = '<ul>' + items + '</ul>';
+        return str
+      }).join('')
+      const list = `<ul>${items}</ul>`
 
       // Generate footer html
-      var footer = '<div class="footer">' + '<a href="http://github.com/amio/gh-index">gh-index</a> ' + 'by <a href="http://github.com/amio">amio</a></div>';
+      const footer = '<div class="footer">' +
+        '<a href="http://github.com/amio/gh-index">gh-index</a> ' +
+        'by <a href="http://github.com/amio">amio</a></div>'
 
       // insert html
-      wrapper.innerHTML = header + list + footer;
+      wrapper.innerHTML = header + list + footer
     }
-  };
+  }
 
-  index.init();
-});
+  index.init()
+})
